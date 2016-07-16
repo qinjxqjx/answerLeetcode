0f88a8dd9f7ffe4e6f5007405c1ce4c3d24719fd
qinjxqjx <qinjxqjx@163.com>
2016-07-16 10:48:15 +0800

    已找到错误的位置及原因

--- p65Fault.cpp
+++ p65Fault.cpp
@@ -6,17 +6,23 @@ void sortAges(int ages[], int length){
 	if (ages == NULL || length <= 0)
 		return;
 
-	const int oldestAge = 9;
-	int timesOfAge[oldestAge + 1];
+	const int oldestAge = 99;
+	int timesOfAge[oldestAge + 1] = { 0 };
+
 
 	/*统计每个年龄出现的次数*/
-	for (int i = 0; i <= oldestAge; ++i){
-		timesOfAge[i] = 0;
-		for (int i = 0; i < length; ++i){
+	/*for (int i = 0; i <= oldestAge; ++i){
+		
+		for (int j = 0; j < length; ++j){
 			int age = ages[i];
 			++ timesOfAge[age];
 		}
-	}
+	}*/ 
+	/*错误发生在本部分，其中，olestAges*length >length 所以会出现
+	下表计算超出设定限.更正程序如下所示*/
+
+	for (int i = 0; i < length; ++i)
+		++timesOfAge[ages[i]];
 
 	/*从小到大将年龄写入到原来的存储数字ages中*/
 	int index = 0;
@@ -35,7 +41,7 @@ int main() {
 	sortAges(ages, length);
 	cout << "the ages is  ";
 	for (int i = 0; i < length; ++i)
-		cout << ages[i];
+		cout << ages[i]<<' ';
 	cout << endl;
 
 	//system("pause");
